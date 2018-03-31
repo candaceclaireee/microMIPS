0ab2857fc6a0f9acfa7faa02ced2402d3d69b928
Candace Claire <mercadocandaceclaire@gmail.com>
2018-03-31 11:04:49 +0800

    Initial Commit

---
+++ ARC-ORG MP02/src/mainproj/Driver.java
@@ -0,0 +1,26 @@
+package mainproj;
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Driver extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("../mainproj/MainWindow.fxml"));
+        Scene scene = new Scene(root);
+        stage.setTitle("microMIPS: Sequential Simulator");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
---
+++ ARC-ORG MP02/src/mainproj/MainWindowController.java
@@ -0,0 +1,25 @@
+package mainproj;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+
+public class MainWindowController implements Initializable {
+	
+	@FXML
+	private TextArea codeTextArea;
+	@FXML
+	private ListView<String> opcodeListView;
+	
+	public void initialize(URL url, ResourceBundle rb) {
+		
+	}
+	
+	@FXML
+	public void processTextArea() {
+		
+	}
+}
